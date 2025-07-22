71c75aaca9d76d3cd1dc2adae17129fda5bf3895
Tim-Mer <timmymerriman@gmail.com>
2025-07-21 22:30:38 -0700

    Adding some tests

--- dave/functions/get_files_info.py
+++ dave/functions/get_files_info.py
@@ -1,7 +1,14 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    if os.path.abspath(directory) not in os.path.join(working_directory):
+    working_directory = os.path.abspath(os.path.join("./", working_directory))
+    directory = os.path.abspath(os.path.join(working_directory, directory))    
+    
+    
+    if os.path.commonpath([working_directory]) != os.path.commonpath([working_directory, directory]):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(directory):
+        return f'Error: "{directory}" is not a directory'
+
     
     
